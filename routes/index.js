574cf1297026dd22f391d0fba519a7e2b6cc62ba
Deuk_Hoi <dh97k@naver.com>
2020-03-02 15:18:24 +0900

    forth version

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hi HoduDeuk' });
+  res.render('index', { title: 'Hi HoduDeuk!' });
 });
 
 module.exports = router;
