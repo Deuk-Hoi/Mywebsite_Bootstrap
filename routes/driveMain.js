c7fd2bc463e8608dc2d23586029a4979c0ff4996
Deuk_Hoi <dh97k@naver.com>
2020-09-14 02:24:07 +0900

    Add Delete function router

--- routes/driveMain.js
+++ routes/driveMain.js
@@ -1,14 +1,15 @@
 var express = require('express');
 const checkDiskSpace = require('check-disk-space');
 var multer = require('multer')
-let _storage = multer.diskStorage({
+var _storage = multer.diskStorage({
   destination: function(req, file ,callback){
-      callback(null, `upload/${req.body.saveUser}/`)
+      callback(null, `upload/${req.body.saveUser}`)
   },
   filename: function(req, file, callback){
       callback(null, req.body.filename)
   }
 })
+
 let upload = multer({storage: _storage})
 var fs = require('fs');
 var router = express.Router();
@@ -43,6 +44,7 @@ router.get('/show', function(req, res, next) {
 
 router.post('/create', upload.single("myFile"), function(req, res, next) {
     res.send("Upload");
+    console.log("hello");
     console.log(req.file)
 });
 
@@ -50,44 +52,37 @@ router.post('/checkExistFolder', (req, res)=>{
     var foldername = req.body.foldername;
     console.log(foldername);
     var json;
-    var state = 1;
 
     function mkdir( dirPath ) {
       const isExists = fs.existsSync( dirPath );
       if( !isExists ) {
           fs.mkdirSync( dirPath, { recursive: true } );
-          state = 1;
+          json = JSON.parse(`{"state" : "${1}"}`);
       }else{
-        state = 0;
+        json = JSON.parse(`{"state" : "${0}"}`)
       }
     }
     mkdir('upload/'+foldername+'/');
-    json = JSON.parse(`{"state" : "${state}"}`)
+    
     res.json(json);
 })
 
-
-
-/*router.post('/uploads', upload.single('myfile'), (req, res, next)=>{
-  console.log("aaaaa");
-  const file = req.file
-  if(!file){
-    const error = new Error('please upload a file');
-    error.httpStatusCode = 400;
-    console.log("error", 'please upload a file');
-
-    res.send({code:500, msg: 'please upload a file'})
-    return next({code:500, msg: error})
-  }
-  res.send({code : 200, msg : file})
-})*/
-  /*var testFolder = 'C:/Users/5G/Desktop';
-  var fs = require('fs');
-   
-  fs.readdir(testFolder, function(error, filelist){
-    console.log(filelist);
-  }) //파일 리스트 확인*/
-
-    
-
+router.post('/removeFile', (req, res) => {
+  console.log(req.body);
+  var state;
+  fs.access(req.body.FilePath, fs.constants.F_OK, (err) =>{
+    if(err){
+      state = "no";
+    }
+    fs.unlink(req.body.FilePath, (err) => {
+      if(err){
+        state = "no";
+      }else{
+        state = "ok";
+      }
+    });
+  });
+  
+  res.send(`{"state" : "${state}"}`);
+})
 module.exports = router;
